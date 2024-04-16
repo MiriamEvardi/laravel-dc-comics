587d5d30b5cc55e33ed434b57cbc1cbaae1fc30f
MiriamEvardi <miriamevardi97@gmail.com>
2024-04-16 11:38:46 +0200

    add soft delete

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -10,7 +10,7 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}" required>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
             @error('title')
             <div class="invalid-feedback">
                 {{$message}}
